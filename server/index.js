42b26cb33fb26e413220349f966df05f60563054
bkchu <brandon.k.chung@gmail.com>
2018-03-05 17:26:36 -0600

    testing

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const journalController = require(`${__dirname}/controllers/journal_controller`)
 const app = express();
 
 app.use(json());
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static("../build"));
 
 const baseUrl = "/api/journal";
 app.post(baseUrl, journalController.addPost);
