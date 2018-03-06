c706fbaf3a0231d9f849e50ffa057048cd02a73e
bkchu <brandon.k.chung@gmail.com>
2018-03-06 11:38:38 -0600

    push origin master

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./styles/reset.css";
 import "./styles/index.css";
 import "./styles/animation.css";
@@ -8,9 +8,9 @@ import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
 registerServiceWorker();
